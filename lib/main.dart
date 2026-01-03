6be143c2f1f4cb3356fc61890cc3a84c4133b246
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2026-01-03 15:55:53 +0200

    refactor: project structure refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flower_shop/pages/checkout_page.dart';
-import 'package:flutter_flower_shop/pages/confirmation_page.dart';
-import 'package:flutter_flower_shop/pages/home_page.dart';
-import 'package:flutter_flower_shop/pages/intro_page.dart';
-import 'package:flutter_flower_shop/pages/product_page.dart';
+import 'package:flutter_flower_shop/screens/checkout/checkout_page.dart';
+import 'package:flutter_flower_shop/screens/confirmation/confirmation_page.dart';
+import 'package:flutter_flower_shop/screens/home/home_page.dart';
+import 'package:flutter_flower_shop/screens/intro/intro_page.dart';
+import 'package:flutter_flower_shop/screens/product/product_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_flower_shop/services/database.dart';
+import 'package:flutter_flower_shop/data/services/database.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_flower_shop/core/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: '/intro',
+        theme: AppTheme.light(),
         routes: {
           '/intro': (context) => const IntroPage(),
           '/home': (context) => const HomePage(),
