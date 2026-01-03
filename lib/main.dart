5cd5d81155e8f8254addd961907ccd433d4ea490
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2026-01-03 14:04:31 +0200

    feat: refactor main.dart to initialize Firebase and provide product stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flower_shop/pages/checkout_page.dart';
+import 'package:flutter_flower_shop/pages/confirmation_page.dart';
 import 'package:flutter_flower_shop/pages/home_page.dart';
 import 'package:flutter_flower_shop/pages/intro_page.dart';
+import 'package:flutter_flower_shop/pages/product_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_flower_shop/services/database.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,13 +21,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/intro',
-      routes: {
-        '/intro': (context) => const IntroPage(),
-        '/home': (context) => const HomePage(),
-      },
+    return StreamProvider<QuerySnapshot?>.value(
+      value: DatabaseService().getProductsStream(),
+      initialData: null,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/intro',
+        routes: {
+          '/intro': (context) => const IntroPage(),
+          '/home': (context) => const HomePage(),
+          '/product': (context) => const ProductPage(),
+          '/checkout': (context) => const CheckoutPage(),
+          '/confirmation': (context) => const ConfirmationPage(),
+        },
+      ),
     );
   }
 }
